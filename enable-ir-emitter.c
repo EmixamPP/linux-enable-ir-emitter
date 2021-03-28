be7293f17cabe50ab5ab7f542ddddd86b1658d95
maxime <dirksen.maxime@gmail.com>
2021-03-28 15:03:10 +0200

    typo white space

--- enable-ir-emitter.c
+++ enable-ir-emitter.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <linux/uvcvideo.h>
 #include <linux/usb/video.h>
 #include <sys/ioctl.h>
@@ -12,14 +11,14 @@ int main() {
     int result, fd;
     errno = 0;
 
-	const char* device = "/dev/video2"; //your camera path
+    const char* device = "/dev/video2"; //your camera path
     if((fd = open(device, O_WRONLY)) < 0){
         fprintf (stderr, "Unable to open a file descriptor for %s\n", device);
         return EXIT_FAILURE;
     }
 
     //Data Fragment
-    __u8 data[9] = {0x01,0x03,0x02,0x00,0x00,0x00,0x00,0x00,0x00};
+    __u8 data[9] = {0x01, 0x03, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     //You can use my python data-seq-to-hex script for format the data fragment directly in the buffer format.
     //You just have to pass the data fragment in parameter
     //Don't forget to change the buffer size with the value of wLength
@@ -51,7 +50,7 @@ int main() {
            case EFAULT:
                 fprintf(stderr, "The data pointer references an inaccessible memory area.\n");
                 break;
-            case EILSEQ:
+           case EILSEQ:
                 fprintf(stderr, "Illegal byte sequence.\n");
                 break;
         }
