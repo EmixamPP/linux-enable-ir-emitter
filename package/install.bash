d0d03ef7844a502b0d5f64021afc538320510dfb
EmixamPP <dirksen.maxime@gmail.com>
2021-05-28 13:58:45 +0200

    better help

--- package/install.bash
+++ package/install.bash
@@ -11,4 +11,4 @@ SCRIPT_PATH="${BASE_PATH}linux-enable-ir-emitter"
 gcc ${BASE_PATH}enable-ir-emitter.c -o ${BASE_PATH}enable-ir-emitter
 ln -s ${BASE_PATH}linux-enable-ir-emitter $PATH_CMD
 
-printf "[Unit]\nDescription=enable ir emitter\nAfter=multi-user.target suspend.target hibernate.target hybrid-sleep.target suspend-then-hibernate.target\n\n[Service]\nExecStart=$SCRIPT_PATH -rf\n\n[Install]\nWantedBy=multi-user.target suspend.target hibernate.target hybrid-sleep.target suspend-then-hibernate.target" >$SERVICE_ENABLE_IR_PATH
+printf "[Unit]\nDescription=enable ir emitter\nAfter=multi-user.target suspend.target hibernate.target hybrid-sleep.target suspend-then-hibernate.target\n\n[Service]\nExecStart=$SCRIPT_PATH -f\n\n[Install]\nWantedBy=multi-user.target suspend.target hibernate.target hybrid-sleep.target suspend-then-hibernate.target" >$SERVICE_ENABLE_IR_PATH
