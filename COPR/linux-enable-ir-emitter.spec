086fd5e6965698434edb5a7c11f66bbeb8100f1a
EmixamPP <dirksen.maxime@gmail.com>
2021-09-17 19:13:34 +0200

    update for 3.0.0

--- COPR/linux-enable-ir-emitter.spec
+++ COPR/linux-enable-ir-emitter.spec
@@ -4,9 +4,9 @@ Name:     linux-enable-ir-emitter
 Version:  3.0.0
 Release:  1%{?dist}
 Summary:  Enables infrared cameras that are not directly enabled out-of-the box
-
 URL:      https://github.com/EmixamPP/%{name}
 License:  MIT
+
 Source0:  https://github.com/EmixamPP/%{name}/archive/refs/tags/%{version}.tar.gz
 
 BuildRequires: make
@@ -25,31 +25,26 @@ make -C sources/uvc
 
 %install
 # software
-mkdir -p %{buildroot}%{_libdir}/%{name}
 install -Dm 644 sources/*.py -t %{buildroot}%{_libdir}/%{name}/
 
-mkdir -p %{buildroot}%{_libdir}/%{name}/uvc
 install -Dm 755 sources/uvc/*query -t %{buildroot}%{_libdir}/%{name}/uvc/
 install -Dm 755 sources/uvc/*query.o -t %{buildroot}%{_libdir}/%{name}/uvc/
 
-mkdir -p %{buildroot}%{_libdir}/%{name}/command
 install -Dm 644 sources/command/*.py -t %{buildroot}%{_libdir}/%{name}/command/
 
 # boot service
-mkdir -p %{buildroot}%{_prefix}/lib/systemd/system/
 install -Dm 644 sources/%{name}.service -t %{buildroot}%{_prefix}/lib/systemd/system/
 
 # executable
-mkdir -p %{buildroot}%{_bindir}
 chmod +x %{buildroot}%{_libdir}/%{name}/%{name}.py
 ln -fs %{_libdir}/%{name}/%{name}.py %{buildroot}%{_bindir}/%{name}
 
 %files
 %license LICENSE
 %doc README.md
-%{_libdir}/%{name}
+%{_libdir}/%{name}/
 %{_prefix}/lib/systemd/system/%{name}.service
-%{_bindir}/%{name}
+%{_bindir}/%{name}/
 
 %postun
 # delete files added after installation
--- installer.sh
+++ installer.sh
@@ -14,32 +14,31 @@ install_dependency() {
 
 do_install() {
     check_root
+    install_dependency
     make -C sources/uvc
 
     # software
-    install -Dm 755 sources/uvc/*query  -t /usr/lib/linux-enable-ir-emitter/uvc/
-    install -Dm 755 sources/uvc/*query.o  -t /usr/lib/linux-enable-ir-emitter/uvc/
+    install -Dm 755 sources/uvc/*query  -t /usr/lib/linux-enable-ir-emitter/uvc/ -v
+    install -Dm 755 sources/uvc/*query.o  -t /usr/lib/linux-enable-ir-emitter/uvc/ -v
 
-    install -Dm 644 sources/command/*.py -t /usr/lib/linux-enable-ir-emitter/command/
+    install -Dm 644 sources/command/*.py -t /usr/lib/linux-enable-ir-emitter/command/ -v
 
-    install -Dm 644 sources/*.py -t /usr/lib/linux-enable-ir-emitter/
+    install -Dm 644 sources/*.py -t /usr/lib/linux-enable-ir-emitter/ -v
 
     # boot service
-    install -Dm 644 sources/linux-enable-ir-emitter.service -t /usr/lib/systemd/system/
+    install -Dm 644 sources/linux-enable-ir-emitter.service -t /usr/lib/systemd/system/ -v
 
     # executable
     chmod +x /usr/lib/linux-enable-ir-emitter/linux-enable-ir-emitter.py
     ln -fs /usr/lib/linux-enable-ir-emitter/linux-enable-ir-emitter.py /usr/bin/linux-enable-ir-emitter
-
-    install_dependency
 }
 
 do_uninstall() {
     check_root
 
-    rm -f /usr/bin/linux-enable-ir-emitter
-    rm -rf /usr/lib/linux-enable-ir-emitter/
-    rm -f /usr/lib/systemd/system/linux-enable-ir-emitter.service
+    rm -fv /usr/bin/linux-enable-ir-emitter 
+    rm -rfv /usr/lib/linux-enable-ir-emitter/
+    rm -fv /usr/lib/systemd/system/linux-enable-ir-emitter.service
 }
 
 check_root() {
