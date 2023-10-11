7fac305efe24b138948d918ed97a326f827675e6
EmixamPP <dev@emixam.be>
2023-10-11 14:45:39 +0200

    refactor: handle file descriptor error

--- camera/is-gray-camera.cpp
+++ camera/is-gray-camera.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #include "camera.hpp"
 #include "opencv.hpp"
 
+constexpr unsigned EXIT_FD_ERROR = 126;
+
 /**
  * Check if a camera is in gray scale
  *
@@ -17,18 +19,26 @@ using namespace std;
 int main(int, const char **argv)
 {
     Camera camera(argv[1]);
-    unique_ptr<cv::Mat> frame = camera.read1();
+    try
+    {
+        unique_ptr<cv::Mat> frame = camera.read1();
 
-    if (frame->channels() != 3)
-        return 1;
+        if (frame->channels() != 3)
+            return EXIT_FAILURE;
 
-    for (int r = 0; r < frame->rows; ++r)
-        for (int c = 0; c < frame->cols; ++c)
-        {
-            const cv::Vec3b &pixel = frame->at<cv::Vec3b>(r, c);
-            if (pixel[0] != pixel[1] || pixel[0] != pixel[2])
-                return 1;
-        }
+        for (int r = 0; r < frame->rows; ++r)
+            for (int c = 0; c < frame->cols; ++c)
+            {
+                const cv::Vec3b &pixel = frame->at<cv::Vec3b>(r, c);
+                if (pixel[0] != pixel[1] || pixel[0] != pixel[2])
+                    return EXIT_FAILURE;
+            }
 
-    return 0;
+        return EXIT_SUCCESS;
+    }
+    catch (CameraException &e)
+    {
+        cerr << e.what() << endl;
+        return EXIT_FD_ERROR;
+    }
 }
