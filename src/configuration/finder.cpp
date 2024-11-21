43a09ffed176e0adf5b6fd7abee542c3a5ac6728
EmixamPP <dev@emixam.be>
2024-11-21 18:49:00 +0100

    style(Finder): split find function

--- src/configuration/finder.cpp
+++ src/configuration/finder.cpp
@@ -30,12 +30,52 @@ static void catch_ctrl_c() {
   });
 }
 
+/**
+ * @brief Search a control that enables the emitter for a given instruction
+ * @param camera the camera on which apply the instruction
+ * @param instruction the instruction on which the search
+ * @param neg_answer_limit the number of negative answers before stopping the search
+ * @return true if a control value enabling the emitter is not found, otherwise false
+ */
+static bool search_ir_control(CameraPtr &camera, CameraInstruction &instruction,
+                              unsigned neg_answer_limit) {
+  // try to set the minimum control value
+  if (!instruction.set_min_cur()) {
+    // if not reset to the initial known value for the instruction
+    instruction.reset();
+  }
+
+  unsigned neg_answer_counter = 0;
+  while (!force_exit && neg_answer_counter < neg_answer_limit && instruction.next()) {
+    cout << '\r' << setw(PROGRESSION_LINE_SIZE) << ' ' << '\r';  // wipe previous
+    cout << "Searching, please be patient"
+         << string(neg_answer_counter % PROGRESSION_NBR_POINTS, '.') << '\r' << flush;
+
+    if (neg_answer_counter == neg_answer_limit - 1) instruction.set_max_cur();
+
+    logger::debug("Instruction applied: {}.", to_string(instruction));
+
+    if (camera->apply(instruction) && camera->is_emitter_working()) {
+      logger::debug("The instruction makes emitter flash.");
+      return true;
+    }
+
+    logger::debug("The instruction does not enable the emitter.");
+    ++neg_answer_counter;
+  }
+
+  instruction.reset();
+  logger::debug("Reseting to the instruction: {}.", to_string(instruction));
+  if (!camera->apply(instruction)) {
+    throw Camera::Exception("Impossible to reset the instruction: {}.", to_string(instruction));
+  }
+  return false;
+}
+
 bool Tools::Find(Configuration &config, unsigned emitters, unsigned neg_answer_limit) {
   catch_ctrl_c();
 
   unsigned configured = 0;
-
-  unsigned p = 0;  // progression
   for (auto &instruction : config) {
     if (instruction.status() == CameraInstruction::Status::DISABLE) {
       logger::debug("Disable instruction skipped: {}.", to_string(instruction));
@@ -43,51 +83,23 @@ bool Tools::Find(Configuration &config, unsigned emitters, unsigned neg_answer_l
     }
 
     try {
-      // try to set the minimum control value
-      if(!instruction.set_min_cur()) {
-        // if not reset to the initial known value for the instruction
-        instruction.reset();
+      if (search_ir_control(config.camera, instruction, neg_answer_limit)) {
+        instruction.set_status(CameraInstruction::Status::START);
+        ++configured;
       }
 
-      unsigned neg_answer_counter = 0;
-      while (!force_exit && neg_answer_counter < neg_answer_limit && instruction.next()) {
-        cout << '\r' << setw(PROGRESSION_LINE_SIZE) << ' ' << '\r';  // wipe previous
-        cout << "Searching, please be patient" << string(p++ % PROGRESSION_NBR_POINTS, '.') << '\r'
-             << flush;
-
-        if (neg_answer_counter == neg_answer_limit - 1) instruction.set_max_cur();
-
-        logger::debug("Instruction applied: {}.", to_string(instruction));
-
-        if (config.camera->apply(instruction) && config.camera->is_emitter_working()) {
-          logger::debug("The instruction makes emitter flash.");
-          instruction.set_status(CameraInstruction::Status::START);
-          if (++configured == emitters) {
-            logger::debug("All emitters are configured.");
-            return true;
-          }
-        } else
-          logger::debug("The instruction does not enable the emitter.");
-
-        ++neg_answer_counter;
+      if (configured == emitters) {
+        logger::debug("All emitters are configured.");
+        return true;
       }
 
-      instruction.reset();
-      logger::debug("Reseting to the instruction: {}.", to_string(instruction));
-      if (!config.camera->apply(instruction)) {
-        logger::error("Impossible to reset the instruction: {}.", to_string(instruction));
-        logger::info("Please shutdown your computer, then boot and retry.");
-        instruction.set_status(CameraInstruction::Status::DISABLE);
-        return false;
-      }
-
-      if (force_exit) return false;
+      if (force_exit) break;
     } catch (const Camera::Exception &e) {
       logger::error(e.what());
       logger::info("Please shutdown your computer, then boot and retry.");
       instruction.reset();
       instruction.set_status(CameraInstruction::Status::DISABLE);
-      return false;
+      break;
     }
   }
 
