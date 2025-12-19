11fac82dbdf772aa50874a4c7fd0a9f5999c1d95
EmixamPP <dev@emixam.be>
2025-12-19 23:02:55 +0100

    refactor: separate UI keys (custom type around crossterm keys) from controller keys (raw crossterm keys)
    for allowing more UI choices while keeping the abstraction at the controller side

--- src/configure/ui.rs
+++ src/configure/ui.rs
@@ -3,55 +3,40 @@ use helper::*;
 pub mod ir_enabler;
 mod shared;
 use shared::*;
+pub use shared::{DeviceSettingsCtx, SearchSettingsCtx};
 pub mod tool_menu;
+pub mod tweaker;
 
-pub mod keys {
+mod keys {
     use crossterm::event::KeyCode;
-    use derive_more::{From, Into};
     use ratatui::{
         style::Stylize,
         style::{Color, Style},
         text::{Line, Span},
     };
 
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, From, Into)]
-    pub(crate) struct Key(pub KeyCode);
+    #[derive(Debug, Clone, Copy)]
+    pub struct Key {
+        code: KeyCode,
+        name: &'static str,
+        color: Color,
+    }
+
+    impl Key {
+        const fn new(code: KeyCode, name: &'static str, color: Color) -> Self {
+            Self { code, name, color }
+        }
+    }
 
     pub fn keys_to_line(keys: &[Key]) -> Line<'static> {
         let mut spans = Vec::with_capacity(keys.len() * 3);
         for (i, key) in keys.iter().enumerate() {
-            match key.0 {
-                KeyCode::Esc => {
-                    spans.push("Quit <".bold());
-                    spans.push(Span::styled("Esc", Style::default().fg(Color::Red)));
-                    spans.push(">".bold());
-                }
-                KeyCode::Tab => {
-                    spans.push("Navigate <".bold());
-                    spans.push(Span::styled("Tab", Style::default().fg(Color::Yellow)));
-                    spans.push(">".bold());
-                }
-                KeyCode::Enter => {
-                    spans.push("Continue <".bold());
-                    spans.push(Span::styled("Enter", Style::default().fg(Color::Green)));
-                    spans.push(">".bold());
-                }
-                KeyCode::Char('y') => {
-                    spans.push("Yes <".bold());
-                    spans.push(Span::styled("y", Style::default().fg(Color::Green)));
-                    spans.push(">".bold());
-                }
-                KeyCode::Char('n') => {
-                    spans.push("No <".bold());
-                    spans.push(Span::styled("n", Style::default().fg(Color::Red)));
-                    spans.push(">".bold());
-                }
-                _ => {
-                    spans.push("? <".bold());
-                    spans.push(Span::raw(format!("{:?}", key.0)));
-                    spans.push(">".bold());
-                }
-            }
+            spans.push(format!("{} <", key.name).bold());
+            spans.push(Span::styled(
+                key.code.to_string(),
+                Style::default().fg(key.color),
+            ));
+            spans.push(">".bold());
 
             if i != keys.len() - 1 {
                 spans.push(Span::raw("   "));
@@ -60,12 +45,11 @@ pub mod keys {
         Line::from(spans)
     }
 
-    pub const KEY_EXIT: Key = Key(KeyCode::Esc);
-    pub const KEY_NAVIGATE: Key = Key(KeyCode::Tab);
-    pub const KEY_CONTINUE: Key = Key(KeyCode::Enter);
-    pub const KEY_YES: Key = Key(KeyCode::Char('y'));
-    pub const KEY_NO: Key = Key(KeyCode::Char('n'));
-    pub const KEY_DELETE: Key = Key(KeyCode::Backspace);
+    pub const KEY_EXIT: Key = Key::new(KeyCode::Esc, "Quit", Color::Red);
+    pub const KEY_NAVIGATE: Key = Key::new(KeyCode::Tab, "Navigate", Color::Yellow);
+    pub const KEY_CONTINUE: Key = Key::new(KeyCode::Enter, "Continue", Color::Green);
+    pub const KEY_YES: Key = Key::new(KeyCode::Char('y'), "Yes", Color::Green);
+    pub const KEY_NO: Key = Key::new(KeyCode::Char('n'), "No", Color::Red);
 }
 
 #[cfg(test)]
